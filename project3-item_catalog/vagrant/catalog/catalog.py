947515816851d26348c8dc46f357ea506fd5ee8b
Paul Tai <javatai@gmail.com>
2015-06-16 23:08:16 -0400

    Fix: Changed some function names due to conflicts/typos in catalog.py

--- project3-item_catalog/vagrant/catalog/catalog.py
+++ project3-item_catalog/vagrant/catalog/catalog.py
@@ -18,33 +18,25 @@ session = DBSession()
 
 @app.route('/login/')
 def userLogin():
-	"""TODO"""
-	pass
+    """TODO"""
+    pass
 
 @app.route('/createuser/')
 def createUser():
-	"""TODO"""
+    """TODO"""
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-	return "Hello World"
+    return "Hello World"
 
 @app.route('/cuisine/<int:cuisine_id>/')
 def cuisineDishes(cuisine_id):
     cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
     if not cuisine:
-		return None
-	dishes = session.query(FoodItem).filter_by(cuisine_id=restaurant.id)
-    for i in dishes:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+        return None
+    dishes = session.query(FoodItem).filter_by(cuisine_id=cuisine.id)
+    return "todo"
 
 
 @app.route('/cuisine/new/')
@@ -53,43 +45,43 @@ def newCuisine():
 
 
 @app.route('/cuisine/<int:cuisine_id>/delete')
-def deleteMenuItem(restaurant_id, menu_id):
-	cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
+def deleteCuisine(restaurant_id, menu_id):
+    cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
     if not cuisine:
-		return None
+        return None
     return "page to delete a cuisine type"
 
 
 @app.route('/cuisine/<int:cuisine_id>/new')
-def newCuisine(cuisine_id):
-	cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
+def newDish(cuisine_id):
+    cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
     if not cuisine:
-		return None
+        return None
     return "page to create a new dish type"
 
 
 @app.route('/cuisine/<int:cuisine_id>/<int:dish_id>/edit')
 def editDish(cuisine_id, dish_id):
-	cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
+    cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
     if not cuisine:
-		return None
-	dishes = session.query(FoodItem).filter_by(cuisine_id=restaurant.id)
-	if not dishes:
-		return None
+        return None
+    dishes = session.query(FoodItem).filter_by(cuisine_id=restaurant.id)
+    if not dishes:
+        return None
     return "page to edit a dish"
 
 
 @app.route('/cuisine/<int:cuisine_id>/<int:dish_id>/delete')
-def deleteMenuItem(cuisine_id, dish_id):
-	cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
+def deleteDish(cuisine_id, dish_id):
+    cuisine = session.query(Cuisine).filter_by(id=cuisine_id).one()
     if not cuisine:
-		return None
-	dishes = session.query(FoodItem).filter_by(cuisine_id=restaurant.id)
-	if not dishes:
-		return None
+        return None
+    dishes = session.query(FoodItem).filter_by(cuisine_id=restaurant.id)
+    if not dishes:
+        return None
     return "page to delete a menu item. Task 3 complete!"
 
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run(host='0.0.0.0', port=5000)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
